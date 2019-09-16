dd544307c6adaf8535b59452f26983d395e17fc5
Your Name <dingguang@guoshengtianfeng.com>
2019-09-16 14:29:28 +0800

    案例准备

--- remark.mk
+++ remark.mk
@@ -1,1 +1,4 @@
-火车轨道监测
+时间插件文档地址： https://demo.demohuo.top/jquery/39/3955/demo/
+引用文件： https://demo.demohuo.top/jquery/39/3955/demo/js/plugins/moment.min.js
+https://demo.demohuo.top/jquery/39/3955/demo/js/datepicker.all.js
+https://demo.demohuo.top/jquery/39/3955/demo/css/datepicker.css
